efa4430511eb06130d1606abdbc75a32ab48ca31
Inacio Pimentel <inacio.frick@edu.pucrs.br>
2024-08-19 20:37:02 -0300

    iniciando exercicio 2

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -101,4 +101,13 @@ public class App {
 
         return res;
     }
+
+    public static long maxVal1(long A[], int n) {  
+        long max = A[0];
+        for (int i = 1; i < n; i++) {  
+            if( A[i] > max ) 
+               max = A[i];
+        }
+        return max;
+    }
 }
